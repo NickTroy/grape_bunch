bf0ce88f6155324b40d63937b9bfa67bbcc31d3b
Mykyta Troianov <nicktroyanov@gmail.com>
2016-11-18 15:15:27 +0200

    generate main files for grape and swagger setup, add runtime dependecies

--- grape_bunch.gemspec
+++ grape_bunch.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'grape_bunch'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2016-11-15'
   s.summary     = "Binds together grape dependencies"
   s.description = "Set of grape dependencies which will be installed inside rails app, provides rails generators."
@@ -9,5 +9,10 @@ Gem::Specification.new do |s|
   s.files       = Dir.glob("{lib}/**/*")
   s.homepage    = 'http://rubygems.org/gems/grape_bunch'
   s.license     = 'MIT'
-  s.add_development_dependency 'rails', '~> 5.0'
+  s.add_runtime_dependency 'grape', '0.18.0'
+  s.add_runtime_dependency 'grape-entity', '0.5.1'
+  s.add_runtime_dependency 'grape-swagger', '0.25.0'
+  s.add_runtime_dependency 'grape-swagger-entity', '0.1.4'
+  s.add_runtime_dependency 'grape-swagger-rails', '0.3.0'
+  s.add_runtime_dependency 'hashie-forbidden_attributes', '0.1.1'
 end
--- lib/generators/grape_bunch/install_generator.rb
+++ lib/generators/grape_bunch/install_generator.rb
@@ -3,10 +3,41 @@ require 'rails/generators/base'
 module GrapeBunch
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+      def create_root_api
+        create_file "app/api/api_v1/api.rb", <<-ROOT_API
+module ApiV1
+  class API < Grape::API
+    prefix 'api'
+    version 'v1'
+    format :json
 
-      def create_initializer_file
-        create_file "config/initializers/initializer.rb", "# Add initialization content here"
+    add_swagger_documentation(
+      api_version: 'v1',
+      hide_documentation_path: true,
+      hide_format: true,
+      info: {
+        title: 'API documentation'
+      }
+    )
+  end
+end
+        ROOT_API
+      end
+
+      def mount_routes
+        inject_into_file 'config/routes.rb', after: "Rails.application.routes.draw do\n" do <<-ROUTES
+  mount ApiV1::API => '/'
+  mount GrapeSwaggerRails::Engine => '/swagger'
+        ROUTES
+        end
+      end
+
+      def swagger_initializer
+        create_file "config/initializers/swagger.rb", <<-SWAGGER_INIT
+GrapeSwaggerRails.options.url      = 'api/v1/swagger_doc'
+GrapeSwaggerRails.options.app_name = 'GrapeSwagger'
+GrapeSwaggerRails.options.app_url  = '/'
+        SWAGGER_INIT
       end
     end
   end
